ecc87e81e7a35f46ed36462c2123b60098f3c5af
Mirco Catalano <catalanomircosav@gmail.com>
2026-04-22 16:06:24 +0200

    movimento personaggio

---
+++ Scripts/Character.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+class_name Character
+
+@export var acceleration: int = 80
+@export var max_speed: int = 250
+@export var friction: int = 20
+
+var move_direction: Vector2 = Vector2.ZERO
+
+func _physics_process(_delta: float) -> void: 
+	move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	if move_direction != Vector2.ZERO:
+		move_direction = move_direction.normalized()
+		velocity += move_direction * acceleration
+		velocity = velocity.limit_length(max_speed)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction)
+		
+	move_and_slide()
